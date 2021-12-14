90a4ec2ce0b147abfd290ce38a901f28209a5525
Nezarik-Intmax <nezarikintmax@gmail.com>
2021-12-15 00:47:13 +0500

    Добавьте файлы проекта.

---
+++ AlgoLab1/AlgoLab1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+#define N 60
+int hashFunction(int a){
+    int b = a%1000;
+    return b;
+}
+
+int main()
+{
+    int a[N];
+    for (int i = 0; i < N; i++) {
+        bool stop = false;
+        while (!stop){
+            stop = true;
+            a[i] = rand() % 9000 + 1000;
+            for (int j = 0; j < i; j++){
+                if (a[i] == a[j]) {
+                    stop = false;
+                    break;
+                }
+                else{
+                    stop = true;
+                }
+            }
+        }
+    }
+    for(int i = 0; i < N; i++){
+        if(i % 10 == 0) cout << endl;
+        cout << a[i] << "  ";
+    }
+    cout << "\nend";
+}
